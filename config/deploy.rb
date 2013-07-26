0d915f4c439b691b20d7652e5463a32115ef12d1
vagrant <vagrant@precise64.(none)>
2013-07-26 13:33:45 +0000

    deployment configs

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+require "bundler/capistrano"
+
+server "ec2-54-212-5-64.us-west-2.compute.amazonaws.com", :web, :app, :db, primary: true
+
+set :application, "project"
+set :user, "ubuntu"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :rails_env, "production"
+
+set :scm, "git"
+set :repository, "git://github.com/ivannasya/vendor.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after 'deploy:update_code', 'deploy:symlink_uploads'
+
+namespace :deploy do
+  task :symlink_uploads do
+    run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
+  end
+  
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+    run "cd #{current_path} && bin/bundle exec clockwork lib/clock.rb"
+  end
+
+end
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
