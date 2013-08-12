62106a768009c908657399b70f2a52ccd38f67a3
ivannasya <vagrant@vagrant.(none)>
2013-08-12 09:10:40 +0000

    deploy with sidekiq

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ require 'sidekiq'
 require 'clockwork'
 
 module Clockwork
-  every(1.day, 'Report', :at => '9:10') do
+  every(1.day, 'Report', :at => '9:20') do
    Sidekiq.logger.info "Starting Report"
    Report.report
  end
