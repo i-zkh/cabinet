61f8e3575e61d036ef8a85b46801e815731fc0d5
ivannasya <vagrant@vagrant.(none)>
2013-08-13 07:41:15 +0000

    Deploy with auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,13 @@ class SessionsController < ApplicationController
       vendor = Vendor.where(:auth_key => params[:auth_key]).first
       if vendor
       session[:vendor_id] = vendor.id
+        if session[:auth_token] = ""
+          response = HTTParty.post( "https://izkh.ru/users/sign_in.json",
+          :body => { :user =>  { :email => "iva.anastya@gmail.com", :password => "slastenka3677" }}.to_json,
+          :headers => { 'Content-Type' => 'application/json' })
+          @user = response.parsed_response["user"]["auth_token"]
+          session[:auth_token] = @user
+        end
       redirect_to transactions_url
     else
       render "new"
