1e68943553a387424e9840a9ccc89d119b2a2b4b
ivannasya <vagrant@vagrant.(none)>
2013-08-13 08:30:16 +0000

    time

--- lib/payment/txt_payment.rb
+++ lib/payment/txt_payment.rb
@@ -8,28 +8,29 @@ class TxtPayment < Payment
   def output
     vendors_id = []
     report = []
-
-    @data.each do |data| 
-      vendors_id << data['vendor_id']
-    end
-    vendors_id.uniq!
-    vendors_id.each do |v|
-      report = @data.select { |d| d['vendor_id'] == v.to_i}
-      i = 1
-      outFile = File.new("#{Vendor.find(v).title}.txt", "w")
-      report.each do |d|
-         d.each_value do |value|
-           outFile.write(value)
-           if (i < d.size)
-           outFile.write(";")
-           i += 1
-           end
-          end
+  if @data != []
+      @data.each do |data| 
+        vendors_id << data['vendor_id']
+      end
+      vendors_id.uniq!
+      vendors_id.each do |v|
+        report = @data.select { |d| d['vendor_id'] == v.to_i}
         i = 1
-        outFile.puts
-     end
-     outFile.close
+        outFile = File.new("#{Vendor.find(v).title}.txt", "w")
+        report.each do |d|
+           d.each_value do |value|
+             outFile.write(value)
+             if (i < d.size)
+             outFile.write(";")
+             i += 1
+             end
+            end
+          i = 1
+          outFile.puts
+       end
+       outFile.close
+      end
+      return vendors_id
     end
-    return vendors_id
-  end
+   end
 end
