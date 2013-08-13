5f3a8f48eff254a5b905859c5f3a2507bff4bc8f
ivannasya <vagrant@vagrant.(none)>
2013-08-13 11:36:37 +0000

    helper session_auth1

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ require 'sidekiq'
 require 'clockwork'
 
 module Clockwork
-  every(1.day, 'Report', :at => '11:07') do
+  every(1.day, 'Report', :at => '11:41') do
    Sidekiq.logger.info "Starting Report"
    Report.report
  end
