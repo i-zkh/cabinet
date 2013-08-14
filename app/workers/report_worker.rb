b3259dc36d84c3d11a6a083a22528caa3c110d40
ivannasya <vagrant@vagrant.(none)>
2013-08-14 06:43:24 +0000

    without sidekiq-deploy

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -4,7 +4,11 @@ class ReportWorker
   include Sidekiq::Worker
      def perform
 		vendor = []
- 		auth = "LuNXcS4tAGMgj8xwr7LR"
+	    #response = HTTParty.post( "https://izkh.ru/users/sign_in.json",
+        #	:body => { :user =>  { :email => "iva.anastya@gmail.com", :password => "slastenka3677" }}.to_json,
+        #	:headers => { 'Content-Type' => 'application/json' })
+        #auth = response.parsed_response["user"]["auth_token"]
+        auth = "LuNXcS4tAGMgj8xwr7LR"
 	    request = RequestPayment.new(auth)
 	    report = Report.new(TxtPayment.new(request.get_payment))
 	    vendor_id = report.output_report
