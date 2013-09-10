29a6294a95727110c2abb439b3e4818b788d4933
ivannasya <vagrant@vagrant.(none)>
2013-09-10 19:06:56 +0000

    clock

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create 
-  	if  params[:auth_key] != ""  
-      vendor = Vendor.where(:auth_key => params[:auth_key]).first
+  	if  (params[:auth_key] != "") && (params[:email] != "")
+      vendor = Vendor.where(:auth_key => params[:auth_key], :email => params[:email]).first
       if vendor
         session[:vendor_id] = vendor.id
         redirect_to transactions_url
