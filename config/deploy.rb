9b270e1c5ca5abf136785a8a4340adc3a569fddb
ivannasya <vagrant@vagrant.(none)>
2013-09-11 08:37:14 +0000

    clooock2

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,18 @@ ssh_options[:forward_agent] = true
 set :ssh_options, {:auth_methods => "publickey"}
 set :ssh_options, {:keys => ["/vagrant/project/aws.pem"]}
 
+  def rails_env
+    fetch(:rails_env, false) ? "RAILS_ENV=#{fetch(:rails_env)}" : ''
+  end
+ 
+  def log_file
+    fetch(:clockwork_log_file, "#{current_path}/log/clockwork.log")
+  end
+ 
+  def pid_file
+    fetch(:clockwork_pid_file, "#{current_path}/tmp/pids/clockwork.pid")
+  end
+
 namespace :deploy do
   task :symlink_uploads do
     run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
@@ -26,9 +38,9 @@ namespace :deploy do
 
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
-    cw_pid_file = "#{shared_path}/pids/clockwork.pid"
-    run "if [ -d #{current_path} ] && [ -f #{cw_pid_file} ]; then cd #{current_path} && kill -int $(cat #{cw_pid_file}) 2>/dev/null; else echo 'clockwork was not running' ; fi"
-    run "cd #{current_path} && bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log restart >> log/clockwork.log 2>&1 &"
-    run "ps -eo pid,command | grep clockwork | grep -v grep | awk '{print $1}' > #{cw_pid_file}"
+    run "if [ -d #{current_path} ] && [ -f #{pid_file} ]; then cd #{current_path} && kill -INT `cat #{pid
+_file}` ; fi"
+    run "daemon --inherit --name=clockwork --env='#{rails_env}' --output=#{log_file} --pidfile=#{pid_file
+} -D #{current_path} -- bundle exec clockwork lib/clock.rb"
   end
 end
