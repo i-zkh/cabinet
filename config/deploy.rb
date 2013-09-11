a7ebc10dfb88613fb906b6b1388bfe2fa9072b53
ivannasya <vagrant@vagrant.(none)>
2013-09-11 08:47:48 +0000

    clooock4

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,6 @@ namespace :deploy do
 
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockwork -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log start"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log start"
   end
 end
