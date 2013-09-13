1f2da4a2b841cb3f19d92fc39175747b1a8b32a6
ivannasya <vagrant@vagrant.(none)>
2013-09-13 16:32:22 +0000

    Clock to 16.38

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -28,5 +28,29 @@ class ReportWorker
 	 #    else
 	 #   		ReportMail.no_transactions.deliver
 	 #    end
+	     	vendors_id, @report, @data, error = [], [], [], []
+		@report = GetRequest.report_daily
+	    if @report != []
+			Report.new(AllPayment.new(@report)).output_report 
+			Report.new(Booker.new(@report)).output_report
+			Report.new(Error.new(@report)).output_report
+
+	      @report.each do |report|
+	      	vendors_id << report['vendor_id']
+	      end
+	      vendors_id.uniq.each do |id|
+	      	@data = @report.select { |d| d['vendor_id'] == id.to_i}
+	      	vendor = Vendor.where(id: id, distribution: true).first
+	        case id
+	    	  when 5
+	          	Report.new(TxtCheckAddress.new(@data, id)).output_report
+	          else
+	          	Report.new(TxtPayment.new(@data, id)).output_report
+	        end
+	   		ReportMail.report_to_vendors("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver  	
+	      end
+	    else
+	   		ReportMail.no_transactions.deliver
+	    end
     end
 end
--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ require 'sidekiq'
 require 'clockwork'
 
 module Clockwork
-  every(1.day, 'Report', :at => '16:14') do
+  every(1.day, 'Report', :at => '16:38') do
    Sidekiq.logger.info "Starting Report"
    Report.report
  end
