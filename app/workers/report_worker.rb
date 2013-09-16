cbedfe06b3d2c3f79ac1b9a662242f130d12c049
ivannasya <vagrant@vagrant.(none)>
2013-09-16 17:00:30 +0000

    report14

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -5,7 +5,7 @@ class ReportWorker
 
      def perform
   #   	vendors_id, @report, @data, error = [], [], [], []
-		# @report = GetRequest.report_daily
+		 @report = GetRequest.report_daily
 	 #    if @report != []
 			Report.new(AllPayment.new(@report)).output_report 
 			# Report.new(Booker.new(@report)).output_report
