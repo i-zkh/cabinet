55b7fb5929a1bec6743422d4840022b6b7c1a287
ivannasya <vagrant@vagrant.(none)>
2013-09-26 06:50:02 +0000

    clock

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,11 @@ set :user, "ubuntu"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-set :rails_env, "production"
+
+set :clockwork_roles, :blabla
 set :cw_log_file, "#{current_path}/log/clockwork.log"
 set :cw_pid_file, "#{current_path}/tmp/pids/clockwork.pid"
+set :rails_env, ENV['rails_env'] || ''
 
 set :scm, "git"
 set :repository, "git://github.com/ivannasya/vendor.git"
@@ -28,9 +30,28 @@ namespace :deploy do
 
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
-  	run "if [ -d #{current_path} ] && [ -f #{cw_pid_file} ]; then cd #{current_path} && kill -int $(cat #{cw_pid_file}) 2>/dev/null; else echo 'clockwork was not running' ; fi"
-  	run "cd #{current_path}; nohup bundle exec clockwork config/clockwork.rb -e #{rails_env} >> #{current_path}/log/clockwork.log 2>&1 &", :pty => false
+  end
+end
+
+after "deploy:restart", "clockwork:restart"
+ 
+namespace :clockwork do
+  desc "Stop clockwork"
+  task :stop, :roles => :app, :on_no_matching_servers => :continue do
+    #2>/dev/null skips errors if the file is found but process is not running for some reason
+    run "if [ -d #{current_path} ] && [ -f #{cw_pid_file} ]; then cd #{current_path} && kill -int $(cat #{cw_pid_file}) 2>/dev/null; else echo 'clockwork was not running' ; fi"
+  end
+ 
+  desc "Start clockwork"
+  task :start, :roles => :app, :on_no_matching_servers => :continue do
+    run "cd #{current_path}; nohup bundle exec clockwork config/clockwork.rb -e #{rails_env} >> #{current_path}/log/clockwork.log 2>&1 &", :pty => false
     # get process id
     run "ps -eo pid,command | grep clockwork | grep -v grep | awk '{print $1}' > #{cw_pid_file}"
+  end
+ 
+  desc "Restart clockwork"
+  task :restart, :roles => #{clockwork_roles}, :on_no_matching_servers => :continue do
+    stop
+    start
   end
 end
