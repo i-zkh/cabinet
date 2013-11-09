915d62ba3775162d48db9f2371897227612ba8e6
ivannasya <vagrant@vagrant.(none)>
2013-11-09 21:28:57 +0000

    clock 2

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,7 @@ class PaymentsController < ApplicationController
 		@report = GetRequest.report_daily
 	    if @report != []
 			# Report.new(AllPayment.new(@report)).output_report
-			Report.new(Booker.new(@report)).output_report
+			# Report.new(Booker.new(@report)).output_report
 			# Report.new(Error.new(@report)).output_report
 
 	      @report.each do |report|
@@ -21,7 +21,7 @@ class PaymentsController < ApplicationController
 		        else
 		          	Report.new(TxtPayment.new(@data, id)).output_report
 		        end
-		   		ReportMail.report("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver unless File.zero?("#{vendor.title}.txt")
+		   		# ReportMail.report("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver unless File.zero?("#{vendor.title}.txt")
 	   		end
 	      end
 	    else
