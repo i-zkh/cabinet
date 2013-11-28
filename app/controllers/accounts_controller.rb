6a289946d192f456ac63f193b2db0361801251e4
ivannasya <vagrant@vagrant.(none)>
2013-11-28 11:09:56 +0000

    report

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -75,16 +75,15 @@ class AccountsController < ApplicationController
 
 	############# Getter.new(Xls.new("report/10-2013/ЖСК199.xls", 63)).update
 	
-	CheckEmail.get_organizations
+	# CheckEmail.get_organizations
 
 	# @g = Getter.new(Xls.new("report/10-2013/11.xls", 58)).input
 	# Osmp.check("0034586", DateTime.now)
 
 
-	# Getter.new(Dbf.new("report/11-2013/сов 147.DBF", 111)).create
+	
 	# Getter.new(Xls.new("report/11-2013/ТСЖ 247 б.xls", 62)).input
 	# Getter.new(Ods.new("report/11-2013/privol.ods", 49)).input
-	# Getter.new(Xls.new("report/11-2013/Спорт3 реестр 10 13.xls", 112)).input	
 	# Getter.new(Xls.new("report/11-2013/Лицевые Счета ЖСК №29.xls", 109)).input
 	# Getter.new(Xls.new("report/11-2013/bus_center.xls", 110)).input
 
@@ -101,6 +100,23 @@ class AccountsController < ApplicationController
 
 	# Перспектива
 	# Getter.new(Xls.new("report/11-2013/ТСЖ Перспектива.xls", 114)).create
+
+
+	Getter.new(Dbf.new("report/11-2013/сов 147.DBF", 112)).create
+	Getter.new(Xls.new("report/11-2013/Спорт3 реестр 10 13.xls", 111)).create
+	Getter.new(Xls.new("report/11-2013/серебрен ручей.xls", 42)).create
+	Getter.new(Xls.new("report/11-2013/промышленный № 261.xls", 15)).create
+	Getter.new(Xls.new("report/11-2013/254.xls", 58)).create
+	Getter.new(Xls.new("report/11-2013/ивушка.xls", 56)).create
+	Getter.new(Xls.new("report/11-2013/КЖСК № 298.xls", 50)).create
+	Getter.new(Xls.new("report/11-2013/ЖСК 138 выгрузка.xls", 120)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 136 выгрузка.xls", 122)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 100 выгрузка.xls", 126)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 96 выгрузка.xls", 124)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 9 выгрузка.xls", 125)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 11 выгрузка.xls", 119)).create
+	Getter.new(Xls.new("report/11-2013/ТСЖ Советский 137 выгрузка.xls", 123)).create
+	Getter.new(Xls.new("report/11-2013/Выгрузка ЖСК-265, ЛС.xls", 127)).create
 
     render json: true
   end
--- lib/parser/xls.rb
+++ lib/parser/xls.rb
@@ -8,14 +8,17 @@ class Xls < Parser
 
 	def input
 		@data = case @vendor_id
-				when 38, 46, 63, 59 						then standard
-				when 55, 47, 67, 62, 109, 110, 111, 114 	then two_columns
-				when 92										then sokol
-				when 93										then debt_account
-				when 58 									then three_columns
-				when 15										then other
-				when 56 									then first_four
-				when 54 									then receipt
+				when 38, 46, 63, 59, 50 						then standard
+				when 55, 47, 67, 62, 109, 110, 111, 114		 	then two_columns
+				when 92											then sokol
+				when 93											then debt_account
+				when 58 										then three_columns
+				when 15											then other
+				when 56 										then first_four
+				when 54 										then receipt
+				when 42											then silver_creek
+				when 120, 118, 119, 122, 123, 124, 125, 126		then one_column
+				when 127										then one_column2
 				else
 					error = "Xls parser don't have method for #{Vendor.find(@vendor_id).title}. Vendor id: #{@vendor_id}"
 					ReportMail.error(error, "[ERROR] Xls parser").deliver
@@ -32,15 +35,16 @@ class Xls < Parser
 	  	super
 	end
 
-	# жск_219, ТСЖ "Уют" (Демократическая, 2), жск 199, 249
+	# жск_219, ТСЖ "Уют" (Демократическая, 2), жск 199, 298 
 	def standard
 	    s = Roo::Excel.new(@file)
 		key, data, address, hash = ["user_account", "city", "street", "building", "apartment", "invoice_amount"], [], [], {}
 
 		(7..s.last_row).each do |i|
 			next if s.cell(i, 2) == 'ИТОГО' || s.cell(i, 2) == nil
-			address = s.cell(i, 3).to_s.gsub!(", ", '-').to_s.split("-")
-			hash = {key[0] => s.cell(i, 2).gsub!(" ", "").to_i, key[1] => "Самара", key[2] => address[0], key[3] => address[1], key[4] => address[2].to_i, key[5] => s.cell(i, s.last_column)}
+			p address = s.cell(i, 3).to_s.split(",")
+			  building = address[1].to_s.split("-")
+			hash = {key[0] => s.cell(i, 2).gsub!(" ", "").to_i, key[1] => "Самара", key[2] => address[0], key[3] => building[0], key[4] => building[1].to_i, key[5] => s.cell(i, s.last_column)}
 		  	data << hash
 		end
 		data
@@ -55,6 +59,20 @@ class Xls < Parser
       		amount = s.cell(i, 5).nil? ? s.cell(i, 4) : s.cell(i, 5)
       		s.cell(i, 3) =~ /(\d+)/
       		hash =  {key[0] => $1, key[1] => amount}
+  	  		data << hash
+  		end
+    	data	
+	end
+
+	# Серебрянный ручей
+	def silver_creek
+		s = Roo::Excel.new(@file)
+	  	key, data, hash = ["user_account", "invoice_amount"], [], {}
+
+  		(8..s.last_row-2).each do |i|
+  			next if s.cell(i, 2) == nil
+      		s.cell(i, 3) =~ /(\d+)/
+      		hash =  {key[0] => $1, key[1] => s.cell(i, s.last_column)}
   	  		data << hash
   		end
     	data	
@@ -78,8 +96,9 @@ class Xls < Parser
 	    s = Roo::Excel.new(@file)
 		  	key, data, hash = ["user_account", "invoice_amount"], [], {}
 
-	  	(3..s.last_row).each do |i|
-	  	  	hash =  {key[0] => s.cell(i, 1).to_i, key[1] => s.cell(i, 3)}
+	  	(2..s.last_row).each do |i|
+	  		next if s.cell(i, 1) == 'Итого'
+	  	  	hash =  {key[0] => s.cell(i, 1).to_i, key[1] => s.cell(i, 2)}
 	  	  	data << hash
 	  	end
 	    data
@@ -115,16 +134,16 @@ class Xls < Parser
 	def other
 		s = Roo::Excel.new(@file)
 		key, data, address, hash = ["user_account", "city", "street", "building", "apartment", "invoice_amount"], [], [], {}
-
-		(2..s.last_row).each do |i|
+		(3..s.last_row).each do |i|
+			next if s.cell(i, 1) == 'ЗАО "Самарагорэнергосбыт"' || s.cell(i, 1) == "Невыясненные платежи" || s.cell(i, 1) == "ОАО \"ПТС\"" || s.cell(i, 1) == "ООО \"Самарские коммунальные системы\"" || s.cell(i, 1) == "Поволжский СБ РФ" || s.cell(i, 1) == "Итого"
 			address = s.cell(i, 1).to_s.split(" ")
-			hash = {key[0] => address[3].to_i, key[1] => "Самара", key[2] => "Губанова", key[3] => address[1], key[4] => address[3], key[5] => s.cell(i, 2)}
+			hash = {key[0] => address[3].to_i, key[1] => "Самара", key[2] => "Губанова", key[3] => address[1].to_i, key[4] => address[3], key[5] => s.cell(i, 2)}
 		  	data << hash
 		end
 		data
 	end
 
-	# ТСЖ №275
+	# ТСЖ №275 квитанции
 	def receipt
 		s = Roo::Excel.new(@file)
 		key, data, address, array, hash = ["user_account", "city", "street", "building", "apartment", "invoice_amount"], [], [], [], {}
@@ -138,6 +157,29 @@ class Xls < Parser
 		hash = {key[0] => address[1], key[1] => "Самара", key[2] => address[0], key[3] => array[0], key[4] => array[1], key[5] => s.cell(19, 9)}
 		   data << hash
 		data
+	end
+
+	# 138 Only for Samara
+	def one_column
+		s = Roo::Excel.new(@file)
+		key, data, address, hash = ["user_account", "city", "street", "building", "apartment", "invoice_amount"], [], [], {}
+		address = s.cell(1, 1).to_s.split(",")
+		(2..s.last_row).each do |i|
+			hash = {key[0] => s.cell(i, 1).to_i, key[1] => "Самара", key[2] => address[0], key[3] => address[1], key[4] => s.cell(i, 1).to_i, key[5] => ""}
+		  	data << hash
+		end
+		data
+	end
+
+	def one_column2
+		s = Roo::Excel.new(@file)
+		key, data, hash = ["user_account"], [], {}
+
+	  	(2..s.last_row).each do |i|
+	  	  	hash =  {key[0] => s.cell(i, 2).to_i}
+	  	  	data << hash
+	  	end
+	    data
 	end
 	
 end
