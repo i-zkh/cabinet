9c81a65a771ac0822aadb9037aafd073e51801c0
ivannasya <vagrant@vagrant.(none)>
2013-12-03 08:20:55 +0000

    amount

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,24 +4,65 @@ class AccountsController < ApplicationController
   def create
   	# 5, 33, 38, 40, 46, 47, 55, 58, 59, 61, 63, 64, 65, 67, 92, 93, 99
 
-	# DataProcessing.amount_to_service(Getter.new(TxtParser.new("report/10-2013/Цифрал-Самара.TXT")).input_data, 5)
-	# DataProcessing.amount_from_db(5)
-	# DataProcessing.amount_from_db(33)
-	# DataProcessing.amount_from_db(40)
-	# DataProcessing.amount_from_db(38)
-	# DataProcessing.amount_from_db(46)
-	# DataProcessing.amount_from_db(55)
-	# DataProcessing.amount_from_db(47)
-	# DataProcessing.amount_from_db(58)
-	# DataProcessing.amount_from_db(61)
-	# DataProcessing.amount_from_db(59)
-	# DataProcessing.amount_from_db(63)
-	# DataProcessing.amount_from_db(64)
-	# DataProcessing.amount_from_db(65)
-	# DataProcessing.amount_from_db(67)
-	# DataProcessing.amount_from_db(92)
-	# DataProcessing.amount_from_db(93)
-	# DataProcessing.amount_from_db(99)
+  	# Getter.new(Xls.new("report/11-2013/ТСЖ 247 б.xls", 62)).input
+	# Getter.new(Ods.new("report/11-2013/privol.ods", 49)).input
+	# Getter.new(Xls.new("report/11-2013/Лицевые Счета ЖСК №29.xls", 109)).input
+	# Getter.new(Xls.new("report/11-2013/bus_center.xls", 110)).input
+
+	# квитанции ТСЖ №275
+	# Dir.foreach('report/11-2013/ТСЖ 275') do |file|
+		# next if file == '.' or file == '..'
+		# Getter.new(Xls.new("report/11-2013/ТСЖ 275/™Ґ®в†≠ж®п 221-6≠Ѓп°ам.xls", 54)).input
+	# end
+
+	# @g = Getter.new(Pdf.new("report/11-2013/Серебряный ручей.pdf", 42)).input
+	
+	#ЖСК-295
+	# Getter.new(Dbf.new("report/11-2013/N0011310.DBF", 113)).input
+
+	# Перспектива
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Перспектива.xls", 114)).create
+
+
+	# Getter.new(Dbf.new("report/11-2013/сов 147.DBF", 112)).create
+	# Getter.new(Xls.new("report/11-2013/Спорт3 реестр 10 13.xls", 111)).create
+
+	# Getter.new(Xls.new("report/11-2013/промышленный № 261.xls", 15)).create
+	# Getter.new(Xls.new("report/11-2013/254.xls", 58)).create
+	# Getter.new(Xls.new("report/11-2013/ивушка.xls", 56)).create
+	# Getter.new(Xls.new("report/11-2013/КЖСК № 298.xls", 50)).create
+	# Getter.new(Xls.new("report/11-2013/ЖСК 138 выгрузка.xls", 120)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 136 выгрузка.xls", 122)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 100 выгрузка.xls", 126)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 96 выгрузка.xls", 124)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 9 выгрузка.xls", 125)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 11 выгрузка.xls", 119)).create
+	# Getter.new(Xls.new("report/11-2013/ТСЖ Советский 137 выгрузка.xls", 123)).create
+	# Getter.new(Xls.new("report/11-2013/Выгрузка ЖСК-265, ЛС.xls", 127)).create
+
+	# Getter.new(Ods.new("report/11-2013/приволжское.ods", 49)).create
+	# Getter.new(Xls.new("report/11-2013/ручей.xls", 42)).create
+
+	DataProcessing.amount_from_db(62)
+	DataProcessing.amount_from_db(49)
+	DataProcessing.amount_from_db(109)
+	DataProcessing.amount_from_db(110)
+	DataProcessing.amount_from_db(113)
+	DataProcessing.amount_from_db(114)
+	DataProcessing.amount_from_db(112)
+	DataProcessing.amount_from_db(111)
+	DataProcessing.amount_from_db(15)
+	DataProcessing.amount_from_db(58)
+	DataProcessing.amount_from_db(56)
+	DataProcessing.amount_from_db(50)
+	DataProcessing.amount_from_db(120)
+	DataProcessing.amount_from_db(126)
+	DataProcessing.amount_from_db(122)
+	DataProcessing.amount_from_db(124)
+	DataProcessing.amount_from_db(125)
+	DataProcessing.amount_from_db(119)
+	DataProcessing.amount_from_db(123)
+	DataProcessing.amount_from_db(127)
 	
 	# Add жск_219, ТСЖ "Уют" (Демократическая, 2), жск 199
 	# DataProcessing.push_data_to_account(Getter.new(Xls.new("report/9-2013/жск_219.xls")).input_data, 38)
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,7 @@ class PaymentsController < ApplicationController
   	def create
     	vendors_id, @report, @data, error = [], [], [], []
 		@report = GetRequest.report_daily
+		@report = @report.select { |d|  d['amount'] > 5 }
 	    if @report != []
 			Report.new(AllPayment.new(@report)).output_report
 			Report.new(Booker.new(@report)).output_report
