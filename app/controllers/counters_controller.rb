298ed08057f1a044117ec12e656acf6ccc877bed
ivannasya <vagrant@vagrant.(none)>
2013-12-05 13:18:21 +0000

    field_title

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -2,7 +2,7 @@ class CountersController < ApplicationController
   before_filter :authorize, only: [:index, :create]
 
   def index
-    array = []
+    array, field_title = [], []
     @path = "http://izkh.ru/"
     if session[:vendor_id] == 45
       @report = GetRequest.meters(1000, DateTime.now.month)
@@ -12,32 +12,41 @@ class CountersController < ApplicationController
     if @report
       @report.each do |r|
         array << r['user_account'].to_s
+        field_title << r['field_title']
       end
       @user_account = array.uniq
+      @field_title = field_title.uniq
     end
   end
 
   def create
     if session[:vendor_id] == 45
-      p @report = GetRequest.meters(1000, params[:month_id])
+      @report = GetRequest.meters(1000, params[:month_id])
     else
       @report = GetRequest.meters(session[:vendor_id], params[:month_id])
     end
     @path = "http://izkh.ru/"
-    data, array, @user_account = [], [], []
+    array, @user_account, field_title, @field_title = [], [], [], [], []
     if @report
       @report.each do |r|
         array << r['user_account'].to_s
+        field_title << r['field_title']
       end
       @user_account = array.uniq
+      @field_title = field_title.uniq
+
+      if !params[:field_title].nil?
+        data = []
+        @report.each { |r| params[:field_title].each { |f| data << r if f == r['field_title'] }}
+        @report = data
+      end
+
       if params[:user_account] != ""
-        @report.each do |r|
-          if params[:user_account] == r['user_account']
-            data << r
-          end
-        end
+        data = []
+        @report.each { |r| data << r if params[:user_account] == r['user_account'] } 
         @report = data
       end
     end
+    p params[:field_title]
   end
 end
