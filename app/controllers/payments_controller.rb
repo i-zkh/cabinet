971d1228d2261e65703e5d28d60cb6a3715992bb
ivannasya <vagrant@vagrant.(none)>
2014-01-27 07:37:07 +0000

    some f

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,36 +1,20 @@
 #encoding: utf-8
 class PaymentsController < ApplicationController
 
-  	def create
-    	vendors_id, @report, @data, error = [], [], [], []
+  	# Daily report and errors to out@izkh.ru
+    def create
 		@report = GetRequest.report_daily
-		@report = @report.select { |d|  d['amount'] > 5 }
 	    if @report != []
 			Report.new(AllPayment.new(@report)).output_report
-			Report.new(Booker.new(@report)).output_report
 			Report.new(Error.new(@report)).output_report
-
-	      @report.each do |report|
-	      	vendors_id << report['vendor_id']
-	      end
-	      vendors_id.uniq.each do |id|
-	      	@data = @report.select { |d| d['vendor_id'] == id.to_i }
-	      	vendor = Vendor.where(id: id, distribution: true).first
-	      	if vendor
-		        case id
-		    	when 5, 44, 40
-		          	Report.new(TxtCheckAddress.new(@data, id)).output_report
-		        else
-		          	Report.new(TxtPayment.new(@data, id)).output_report
-		        end
-		   		ReportMail.report("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver unless File.zero?("#{vendor.title}.txt")
-	   		end
-	      end
+			Report.new(Booker.new(@report)).output_report
+			send_report_to_vendors(@report)
 	    else
 	   		ReportMail.no_transactions.deliver
+	   		logger.info "no transactions"
 	    end
-    	render json: @report
-  	end
+	    render json: @report
+    end
 
 	def monthly_xls
 		GetRequest.report_monthly(12).each do |id|
@@ -77,9 +61,9 @@ class PaymentsController < ApplicationController
 		        else
 		          	Report.new(TxtPayment.new(@data, id)).output_report
 		        end
-		   		# ReportMail.report("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver unless File.zero?("#{vendor.title}.txt")
-	   		end
+		   		ReportMail.report("Выгрузка транзакций АйЖКХ за #{Russian::strftime(DateTime.now, "%B " "%Y")}", vendor).deliver unless File.zero?("#{vendor.title}.txt")
+		   		logger.info "transaction: #{vendor.title}-#{@data}"
+	    	end
 	    end
   	end
-
 end
