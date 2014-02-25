a9722f1aebebbb3fe8b9b237019e10afd74f4a22
ivannasya <vagrant@vagrant.(none)>
2014-02-25 11:32:49 +0000

    deploy with clockwork

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ end
 namespace :clockwork do
   desc "Stop clockwork"
   task :stop, :roles => clockwork_roles, :on_error => :continue, :on_no_matching_servers => :continue do
-  run "if [ -d #{current_path} ] && [ -f #{cw_pid_file} ]; then cd #{current_path} && kill -int $(cat #{cw_pid_file}) 2>/dev/null; else echo 'clockwork was not running' ; fi"
+  run "ps -ef | grep PROCESS | grep -v grep | awk '{print $2}' | xargs kill -9"
   end
  
   desc "Start clockwork"
