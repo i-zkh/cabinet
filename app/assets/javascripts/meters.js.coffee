90db3366a1108209dd89aa181782a01bb3d73c0b
ivannasya <vagrant@vagrant.(none)>
2014-02-28 10:56:04 +0000

    Add meters to cadinet

--- app/assets/javascripts/meters.js.coffee
+++ app/assets/javascripts/meters.js.coffee
@@ -7,3 +7,8 @@ jQuery ->
 		monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
 		dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
 		firstDay: 1
+
+toggle = (source) ->
+	checkboxes = document.getElementsByName 'foo'
+	for checkbox in checkboxes
+		checkbox.checked = source.checked
--- app/controllers/organization_controller.rb
+++ app/controllers/organization_controller.rb
@@ -5,7 +5,7 @@ class OrganizationController < ApplicationController
 		if params[:file]
       		filename = "/home/ubuntu/apps/project/shared/organizations/" + "#{Date.today.strftime('%Y-%m-%d')}-" + "#{params[:file].original_filename}"
 			File.open(File.join(filename), "wb") {|f| f.write(params[:file].read)}
-			# Organization.new(filename).add_vendors
+			Organization.new(filename).add_vendors
 		end
 	end
 
