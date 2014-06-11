827995d1b3cbe107a791fad3f925d006d9532400
ivannasya <vagrant@vagrant.(none)>
2014-06-11 07:53:00 +0000

    account

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -4,7 +4,7 @@ class ManagerController < ApplicationController
     @vendor_title = []
     @month = params.has_key?(:date) ? params[:date][:month].to_i : Date.today.month
     @year  = params.has_key?(:date) ? params[:date][:year].to_i  : Date.today.year
-    FileUtils.mkpath "/home/ubuntu/apps/project/shared/report/#{@year}-#{@month}"
+    FileUtils.mkpath "report/#{@year}-#{@month}"
     @vendors_with_report = vendors_with_report
     Vendor.all.each { |v| @vendor_title << v.title }
   end
@@ -13,9 +13,9 @@ class ManagerController < ApplicationController
     @vendors = Vendor.all.select{ |v| !File.exists?(v.title) }
     vendor = Vendor.where(title: params[:vendor]).first
     if params.has_key?(:file) && vendor
-      filename = "/home/ubuntu/apps/project/shared/report/#{DateTime.now.year}-#{DateTime.now.month}/" + "#{vendor.id}" + "#{File.extname("#{params[:file].original_filename}")}"
+      filename = "#{vendor.id}" + "#{File.extname("#{params[:file].original_filename}")}"
       File.open(File.join(filename), "wb") {|f| f.write(params[:file].read)}
-      # WebReportWorker.perform_async(filename, vendor.id)
+      WebReportWorker.perform_async(filename, vendor.id)
       flash[:notice] = "Файл успешно добавлен."
     else
       flash[:notice] = "Необходимо выбрать файл и поставщика."
@@ -25,7 +25,7 @@ class ManagerController < ApplicationController
 
   def vendors_with_report
     vendors_with_report = []
-    directory = params.has_key?(:date) ? "/home/ubuntu/apps/project/shared/report/#{params[:date][:year]}-#{params[:date][:month]}" : "/home/ubuntu/apps/project/shared/report/#{DateTime.now.year}-#{DateTime.now.month}"
+    directory = params.has_key?(:date) ? "report/#{params[:date][:year]}-#{params[:date][:month]}" : "report/#{DateTime.now.year}-#{DateTime.now.month}"
     Dir.foreach(directory) do |file|
       next if file == '.' or file == '..'
       vendors_with_report << Vendor.where(id: File.basename(file, ".*")).first
